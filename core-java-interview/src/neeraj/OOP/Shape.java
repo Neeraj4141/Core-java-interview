678276eef1bd629e05596413a653ed4107ad3f84
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-08-22 22:14:49 +0530

    done

---
+++ core-java-interview/src/neeraj/OOP/Shape.java
@@ -0,0 +1,29 @@
+package neeraj.OOP;
+
+public class Shape {
+
+	public String color;
+	public String name;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double area() {
+		return 0;
+
+	}
+
+}
