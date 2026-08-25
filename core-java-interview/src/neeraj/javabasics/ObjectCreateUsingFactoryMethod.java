191b0c07d2b65e6c788934182a3756184f5a2afd
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-08-25 17:12:36 +0530

    done

---
+++ core-java-interview/src/neeraj/javabasics/ObjectCreateUsingFactoryMethod.java
@@ -0,0 +1,11 @@
+package neeraj.javabasics;
+
+import java.util.Calendar;
+
+public class ObjectCreateUsingFactoryMethod {
+
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		System.out.println("Object created using factory method: " + c.getTime());
+	}
+}
---
+++ core-java-interview/src/neeraj/javabasics/ObjectCreateUsingReflection.java
@@ -0,0 +1,21 @@
+package neeraj.javabasics;
+
+import java.lang.reflect.Constructor;
+
+public class ObjectCreateUsingReflection {
+
+	public void display() {
+		System.out.println("Object created using Constructor.newInstance()");
+	}
+
+	public static void main(String[] args) {
+		try {
+			Constructor<ObjectCreateUsingReflection> constructor = ObjectCreateUsingReflection.class
+					.getDeclaredConstructor();
+			ObjectCreateUsingReflection example = constructor.newInstance();
+			example.display();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
