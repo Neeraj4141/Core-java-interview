f256104c2ff219f1f8c4d263996b0eb8025750c5
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-09-08 10:36:47 +0530

    done

--- core-java-interview/src/neeraj/OOP/shallowdeep/CustomerTest.java
+++ core-java-interview/src/neeraj/OOP/shallowdeep/CustomerTest.java
@@ -12,13 +12,15 @@ public class CustomerTest {
 		cus.acc.balance = 100;
 
 		// Customer ka clone ban raha hai
-		Customer cus1 = (Customer) cus.clone();
+		Customer cus1 = (Customer) cus;
+		
+		cus1.acc.balance = 200;
 
 		System.out.println(cus.name);
 		System.out.println(cus.acc.balance);
 
 		// Cloned Customer ke account ka balance change
-		cus1.acc.balance = 200;
+		
 
 		System.out.println(cus1.name);
 		System.out.println(cus1.acc.balance);
